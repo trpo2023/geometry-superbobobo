e430f0ed2af16060034c0546835c9373a0b7630f
Seperbobobo <pavveltash@gmail.com>
2023-04-29 17:30:12 +0700

    update main

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -1,25 +1,46 @@
-#include <libgeometry/func.h>
+#include <func.h>
+#include <iostream>
+#include <string>
+#include <vector>
 
 int main()
 {
-    FILE* file;
-    file = fopen("../input.txt", "r");
-    
-    if (file == NULL) 
-	{
-        printf("Can't oppening the file");
-        return 1;
-    }
-    
-	char str[N];
-    
-    while (fgets(str, N, file)) 
-	{
-        finaler(str);
-    }
-    fclose(file);
-    
-    return 0;
-   
-}
+    std ::string path = "test";
+    std ::vector<std ::string> str
+            = dataFromFile(path); 
+    int k = str.size();
+    double** prmtrs;
+    prmtrs = new double*[k];
+    for (int i = 0; i < k; i++)
+        prmtrs[i] = new double[3];
 
+    for (int i = 0; i < k; i++) {
+        StrChek(str[i], prmtrs[i]);
+        for (int j = 0; j < 3; j++)
+            std ::cout << prmtrs[i][j] << " ";
+        std ::cout << std ::endl;
+    }
+
+    for (std ::string s : str)
+        std ::cout << s << std ::endl;
+    /////////////////////////////////////////////////////////////////////
+    for (int i = 0; i < k; i++) {
+        if ((prmtrs[i][0] != 0) && (prmtrs[i][1] != 0) && (prmtrs[i][2] != 0)) {
+            std ::cout << "perimeter =" << perimter(prmtrs[i][2]) << std ::endl;
+            std ::cout << "area =" << area(prmtrs[i][2]) << std ::endl;
+            std ::cout << "intersects:" << std ::endl;
+            for (int j = 0; j < k; j++) {
+                if ((prmtrs[i][0] != 0) && (prmtrs[i][1] != 0)
+                    && (prmtrs[i][2] != 0) && (j != i)) {
+                    if (intersect(prmtrs, i, j))
+                        std ::cout << "circle" << std ::endl;
+                }
+            }
+        }
+        std ::cout << std ::endl;
+    }
+    for (int i = 0; i < k; i++) {
+        delete prmtrs[i];
+    }
+    return 0;
+}
